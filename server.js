51dd006c78010085b40bd55b6bd60a1b3bf26e83
joeflacco5 <david.rose@headstartapp.com>
2016-12-17 15:33:35 +0000

    created express skeleton

--- server.js
+++ server.js
@@ -1,27 +1,2 @@
-// Requests flow from Top to Bottom, going through middleware/routes as they go down.
-
-// npm install --save express-handlebars
 var express = require('express');
-var exphbs = require('express-handlebars');
-var bodyParser = require('body-parser');
-
-var app = express();
-
-JSON.stringify({})
-
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
-
-app.get('/', function( req, res ) {
-  res.render('home')
-});
-app.listen(3000, function() {
-    console.log('Working?')
-  });
-
-/* Verb Methods: HTTP requests !
-Get -
-Post -
-Put -
-Delete -
-*/
+app = express(); 
